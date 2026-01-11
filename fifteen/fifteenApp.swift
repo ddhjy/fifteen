339ac58dedaa5e00511cc60fadd3ea658499803e
zengkai <zengkai@bytedance.com>
2026-01-12 00:13:49 +0800

    Initial Commit

---
+++ fifteen/fifteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifteenApp.swift
+//  fifteen
+//
+//  Created by zengkai on 2026/1/12.
+//
+
+import SwiftUI
+
+@main
+struct fifteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
